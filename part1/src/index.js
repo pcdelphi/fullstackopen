038e4a0faed0997eb5fbf666c4443f1eb53cd59b
pcdelphi <pcdelphi@hotmail.com>
2020-09-25 16:49:59 +0800

    index.js中增加动态内容

--- part1/src/index.js
+++ part1/src/index.js
@@ -4,10 +4,16 @@ import ReactDOM from 'react-dom';
 // 定义一个名为App的React组件，从JavaScript来说，就是一个不接受任何参数的函数，() => 
 // 该函数被赋值给一个const修饰的变量App
 const App = () => {
-  console.log('Hello from component')
+  const now = new Date()
+  const a = 10
+  const b = 20
+
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} + {b} = {a +b}
+      </p>
     </div>
   )
 }
