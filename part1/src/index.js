a45c3cfbccadfa6374d013f8b5ac5125f7826cce
pcdelphi <pcdelphi@hotmail.com>
2020-09-25 16:42:57 +0800

    Edit index.js

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// 定义一个名为App的React组件，从JavaScript来说，就是一个不接受任何参数的函数，() => 
+// 该函数被赋值给一个const修饰的变量App
+const App = () => {
+  console.log('Hello from component')
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+ReactDOM.render(<App />, document.getElementById('root'))
